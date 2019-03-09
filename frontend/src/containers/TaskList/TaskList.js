8e574b15db51267827303d5e5caeee87ef7e9341
galubezzz <galubezzz@gmail.com>
2019-03-09 01:27:56 -0800

    add a container for TaskList

--- frontend/src/containers/TaskList/TaskList.js
+++ frontend/src/containers/TaskList/TaskList.js
@@ -11,21 +11,46 @@ const TASKS_URL = 'http://localhost:8000/api/v1/tasks';
 class TaskList extends Component {
     state = {
         tasks: [],
+        backlog: [],
+        in_progress: [],
+        done: []
     };
 
     componentDidMount() {
         axios.get(TASKS_URL)
             .then(response => {console.log(response.data); return response.data;})
-            .then(tasks => this.setState({tasks}))
+            .then(tasks => this.setState({tasks: tasks}))
             .catch(error => console.log(error));
+
+        let tasks = [...this.state.tasks];
+        console.log(tasks);
+
+        let backlog = tasks.filter(function(task) {
+            return task.status === 'backlog';
+        });
+
+        this.setState({backlog: backlog})
+        console.log(backlog);
+
+        let in_progress = tasks.filter(function(task) {
+            return task.status === 'in_progress';
+        });
+        this.setState({in_progress: in_progress})
+
+        let done = tasks.filter(function(task) {
+            return task.status === 'done';
+        });
+        this.setState({done: done})
+
     }
+
 
     render() {
         return <Fragment>
-            <p><NavLink to='/movies/add'>Добавить фильм</NavLink></p>
+            <p><NavLink to='/tasks/add'>Добавить задачу</NavLink></p>
             <div className='row'>
                 {this.state.tasks.map(task => {
-                    return <div className='col-xs-12 col-sm-6 col-lg-4 mt-3'  key={task.id}>
+                    return <div className='col-xs-12 col-sm-6 col-lg-4 mt-3'>
                         <Task task={task}/>
                     </div>
                 })}
