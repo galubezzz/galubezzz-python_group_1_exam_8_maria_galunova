4dd8681f8cde2fb990e27122252093815c0382bf
galubezzz <galubezzz@gmail.com>
2019-03-08 22:03:17 -0800

    fix the viewsets

--- source/api_v1/views.py
+++ source/api_v1/views.py
@@ -1,10 +1,16 @@
 from rest_framework import viewsets
 from webapp.models import Task
 from api_v1.serializers import TaskSerializer
+
+
 # Create your views here.
 
-class TaskViewSet(viewsets.ModelViewSet):
-    queryset = Task.objects.all().order_by('status', '-due_date')
+class NoAuthModelViewSet(viewsets.ModelViewSet):
+    authentication_classes = []
+
+
+class TaskViewSet(NoAuthModelViewSet):
+    queryset = Task.objects.active().order_by('status', '-due_date')
     serializer_class = TaskSerializer
 
     def perform_destroy(self, instance):
