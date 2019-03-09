41a60101d82680726f7de5a4d61d239b1be5604d
galubezzz <galubezzz@gmail.com>
2019-03-08 21:59:36 -0800

    add view sets for task

--- source/api_v1/views.py
+++ source/api_v1/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
-
+from rest_framework import viewsets
+from webapp.models import Task
+from api_v1.serializers import TaskSerializer
 # Create your views here.
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all().order_by('status', '-due_date')
+    serializer_class = TaskSerializer
+
+    def perform_destroy(self, instance):
+        instance.is_deleted = True
+        instance.save()
